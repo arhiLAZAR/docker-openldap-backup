87cda25563a9ebd0afdfd016883957faea5549d0
Anton Evdokimov <evdokimov@selectel.ru>
2021-06-01 19:26:45 +0300

    Parameterize the bucket name and create the "latest" backup

--- scripts/upload_backup.sh
+++ scripts/upload_backup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 unixtime=$(date +%s)
+bucket=${RCLONE_S3_BUCKET_NAME;-ldapbackups}
 
 cd "${BACKUP_TMP_DIR:-/tmp}"
 tar -zvcf tree.ldif.tar.gz tree.ldif
 
-rclone copy tree.ldif.tar.gz s3:/ldap_backups/${unixtime}/tree.ldif.tar.gz
+rclone mkdir s3:/${bucket}
+rclone copy tree.ldif.tar.gz s3:/${bucket}/${unixtime}
+rclone copy tree.ldif.tar.gz s3:/${bucket}/latest
